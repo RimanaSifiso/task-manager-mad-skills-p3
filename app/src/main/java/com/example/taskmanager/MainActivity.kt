7667ad3c0d252b69091b150c3e3f7dff1dd3feff
Sifiso Rimana <rimanasifiso@gmail.com>
2023-12-15 08:11:47 +0200

    Project Complete

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TaskCompleteScreen()
                 }
             }
         }
@@ -30,17 +41,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TaskManagerTheme {
-        Greeting("Android")
+private fun TaskCompleteScreen(modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = stringResource(id = R.string.task_complete),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier=Modifier.padding(top=24.dp, bottom = 8.dp)
+        )
+        Text(text = stringResource(id = R.string.nice_work),
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp
+            )
     }
 }
